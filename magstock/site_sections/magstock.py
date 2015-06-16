651a264271afb9b4d3f0d87528f38ea9e0a9f75b
Eli Courtwright <eli@courtwright.org>
2015-06-16 03:02:50 -0400

    added filters to camp party page

--- magstock/site_sections/magstock.py
+++ magstock/site_sections/magstock.py
@@ -3,7 +3,7 @@ from uber.common import *
 
 @all_renderable(c.PEOPLE)
 class Root:
-    def grouped(self, session):
+    def grouped(self, session, noise=None, site=None, camp=None):
         attendees = session.query(Attendee).all()
 
         names = {}
@@ -24,7 +24,20 @@ class Root:
                 except:
                     pass
 
-        return {'grouped': sorted({frozenset(group) for group in lookup.values()}, key=len, reverse=True)}
+        def match(a):
+            return ((not site or a.site_type == int(site))
+                and (not camp or a.camping_type == int(camp))
+                and (not noise or a.noise_level == int(noise)))
+
+        def any_match(group):
+            return any(match(a) for a in group)
+
+        return {
+            'camp': camp,
+            'site': site,
+            'noise': noise,
+            'grouped': sorted({frozenset(group) for group in lookup.values() if any_match(group)}, key=len, reverse=True)
+        }
 
     def food_purchases(self, session):
         return {'attendees': session.food_purchasers().order_by(Attendee.full_name).all()}
