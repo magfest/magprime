5ce01a8a9a91fbefe36b3241bcecd76303759585
Victoria Earl <kitsuta@gmail.com>
2015-10-28 00:18:14 -0400

    Change sender for Food Suite volunteering email
    Per https://github.com/magfest/ubersystem/issues/1535

--- magprime/automated_emails.py
+++ magprime/automated_emails.py
@@ -23,8 +23,8 @@ GuestEmail('MAGFest hospitality suite information', 'guest_food_info.txt')
 StopsEmail('MAGFest Volunteer Food', 'volunteer_food_info.txt',
            lambda a: days_before(7, c.UBER_TAKEDOWN))
 
-StopsEmail('MAGFest Staff Suite Volunteering', 'food_interest.txt',
-           lambda a: a.requested(c.FOOD_PREP) and not a.assigned_depts)
+AutomatedEmail('MAGFest Staff Suite Volunteering', 'food_interest.txt',
+           lambda a: a.requested(c.FOOD_PREP) and not a.assigned_depts, sender='chefs@magfest.org')
 
 AutomatedEmail(Attendee, 'Important MAGFest PC Gaming Room Information! *PLEASE READ*', 'lan_room.html',
                lambda a: LAN in a.interests_ints,
