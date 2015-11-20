12cd1b2f7a0c89547c0ea5fa9e93e2fc521ba5a2
Eli Courtwright <eli@courtwright.org>
2015-11-19 19:38:32 -0500

    added treasury allotments step to checklist

--- magprime/site_sections/magprime_dept_checklist.py
+++ magprime/site_sections/magprime_dept_checklist.py
@@ -18,3 +18,18 @@ class Root:
             raise HTTPRedirect('../dept_checklist/index?message={}', 'Thanks for completing the Cash/MPoints form!')
 
         return {}
+
+    def allotments(self, session, submitted=None, csrf_token=None, **params):
+        attendee = session.admin_attendee()
+        conf = DeptChecklistConf.instances['allotments']
+        if submitted:
+            try:
+                [item] = [item for item in attendee.dept_checklist_items if item.slug == 'allotments']
+            except:
+                item = DeptChecklistItem(slug='allotments', attendee=attendee)
+            check_csrf(csrf_token)  # since this form doesn't use our normal utility methods, we need to do this manually
+            item.comments = render('magprime_dept_checklist/allotments.txt', params).decode('utf-8')
+            session.add(item)
+            raise HTTPRedirect('../dept_checklist/index?message={}', 'Treasury checklist data uploaded')
+
+        return {}
