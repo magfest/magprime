81c71ee1b0edbbba621930d41324d0130102cb64
Eli Courtwright <eli@courtwright.org>
2015-11-19 20:10:58 -0500

    minor clarifications to treasury checklist items

--- magprime/site_sections/magprime_dept_checklist.py
+++ magprime/site_sections/magprime_dept_checklist.py
@@ -15,7 +15,7 @@ class Root:
                 item = DeptChecklistItem(slug='treasury', attendee=attendee)
             check_csrf(csrf_token)  # since this form doesn't use our normal utility methods, we need to do this manually
             session.add(item)
-            raise HTTPRedirect('../dept_checklist/index?message={}', 'Thanks for completing the Cash/MPoints form!')
+            raise HTTPRedirect('../dept_checklist/index?message={}', 'Thanks for completing the MPoints form!')
 
         return {}
 
