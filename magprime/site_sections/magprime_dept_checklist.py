272cf29a39c7996fa092a15cd7020af12867acfb
Eli Courtwright <eli@courtwright.org>
2015-12-20 21:40:53 -0500

    added techops form to dept checklist

--- magprime/site_sections/magprime_dept_checklist.py
+++ magprime/site_sections/magprime_dept_checklist.py
@@ -33,3 +33,16 @@ class Root:
             raise HTTPRedirect('../dept_checklist/index?message={}', 'Treasury checklist data uploaded')
 
         return {}
+
+    def tech_requirements(self, session, submitted=None, csrf_token=None):
+        attendee = session.admin_attendee()
+        if submitted:
+            try:
+                [item] = [item for item in attendee.dept_checklist_items if item.slug == 'tech_requirements']
+            except:
+                item = DeptChecklistItem(slug='tech_requirements', attendee=attendee)
+            check_csrf(csrf_token)  # since this form doesn't use our normal utility methods, we need to do this manually
+            session.add(item)
+            raise HTTPRedirect('../dept_checklist/index?message={}', 'Thanks for completing the tech requirements form!')
+
+        return {}
