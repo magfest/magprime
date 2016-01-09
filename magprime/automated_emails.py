5c78f114de15c9faecd0de28462e7eafa4e630a5
Nick Marinelli <nick.marinelli@leverege.com>
2016-01-08 20:28:11 -0500

    Added conditions for shirt size confirmation e-mail to be sent.

--- magprime/automated_emails.py
+++ magprime/automated_emails.py
@@ -77,4 +77,5 @@ AutomatedEmail(Attendee, 'Last Chance for MAGFest 2016 bonus swag!', 'attendee_s
 // size deadline (a new deadline not yet recorded in uber).					 
 AutomatedEmail(Attendee, 'MAGFest 2016 t-shirt size confirmation', 'confirm_shirt_size.html',
 				lambda a:  before(datetime(2016, 1, 16, tzinfo=c.EVENT_TIMEZONE) and
-				           false)// HALP!
+						   days_after(3, a.registered) and
+				           a.gets_shirt)
