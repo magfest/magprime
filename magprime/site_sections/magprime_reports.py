810acc1de95892dbc2adb394b348f88b3d845040
Eli Courtwright <eli@courtwright.org>
2016-02-15 14:14:14 -0500

    added report for showing volunteers on shift so department heads will have an easier time ordering food

--- magprime/site_sections/magprime_reports.py
+++ magprime/site_sections/magprime_reports.py
@@ -13,3 +13,29 @@ class Root:
                                           badge_status=c.INVALID_STATUS)
                                .order_by(Attendee.full_name).all()
         }
+
+    def volunteer_food(self, session, message='', department=None, start=None, end=None):
+        staffers = []
+        if cherrypy.request.method == 'POST':
+            start = c.EVENT_TIMEZONE.localize(datetime.strptime(start, c.TIMESTAMP_FORMAT))
+            end = c.EVENT_TIMEZONE.localize(datetime.strptime(end, c.TIMESTAMP_FORMAT))
+            if end < start:
+                message = 'Start must come before end {} {}'.format(start, end)
+            else:
+                hours = set()
+                hour = start
+                while hour < end:
+                    hours.add(hour)
+                    hour += timedelta(hours=1)
+                for job in session.jobs().filter_by(location=department):
+                    if hours.intersection(job.hours):
+                        for shift in job.shifts:
+                            staffers.append(shift.attendee)
+
+        return {
+            'message': message,
+            'end': end,
+            'start': start,
+            'department': department,
+            'staffers': sorted(staffers, key=lambda a: a.full_name)
+        }
