eee9f7043e5503ef66c52a84f099e27db28b1b5f
Eli Courtwright <eli@courtwright.org>
2016-02-17 17:29:22 -0500

    automatically redirect people on the prereg form to the at-the-door reg form

--- magprime/__init__.py
+++ magprime/__init__.py
@@ -62,3 +62,10 @@ from magprime.utils import *
 from magprime.models import *
 from magprime.automated_emails import *
 mount_site_sections(magprime_config['module_root'])
+
+if c.AT_THE_CON:
+    from uber.site_sections.preregistration import Root
+    @cherrypy.expose
+    def form(self, *args, **kwargs):
+        raise HTTPRedirect('../registration/register')
+    Root.form = form
