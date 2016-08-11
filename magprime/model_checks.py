d5da73502b807633e1d4d5a5c79ffcee394eee8e
Victoria Earl <kitsuta@gmail.com>
2016-08-11 11:10:34 -0400

    Fix pep8 Double blank lines, my eternal nemesis.

--- magprime/model_checks.py
+++ magprime/model_checks.py
@@ -18,12 +18,14 @@ def group_leader_under_13(attendee):
     if attendee.badge_type == c.PSEUDO_GROUP_BADGE and attendee.age_group_conf['val'] in [c.UNDER_6, c.UNDER_13]:
         return "Children under 13 cannot be group leaders."
 
+
 @prereg_validation.Attendee
 def total_cost_over_paid(attendee):
     if attendee.total_cost < attendee.amount_paid:
         if attendee.orig_value_of('birthdate') < attendee.birthdate and attendee.age_group_conf['val'] in [c.UNDER_6, c.UNDER_13]:
             return 'The date of birth you entered incurs a discount; please email regsupport@magfest.org to change your badge and receive a refund'
         return 'You have already paid ${}, you cannot reduce your extras below that.'.format(attendee.amount_paid)
+
 
 @validation.Attendee
 def child_badge_over_18(attendee):
