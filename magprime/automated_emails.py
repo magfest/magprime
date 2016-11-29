a967048de454d451628fef5acfd16d080a7e79b5
Dominic Cerquetti <binary1230@gmail.com>
2016-11-28 19:58:22 -0500

    handle filter() when None

--- magprime/automated_emails.py
+++ magprime/automated_emails.py
@@ -15,7 +15,9 @@ for _event in SeasonEvent.instances.values():
     SeasonSupporterEmail(_event)
 
 
-AutomatedEmail(Attendee, 'MAGFest schedule, maps, and other FAQs', 'precon_faqs.html', when=days_before(7, c.EPOCH))
+AutomatedEmail(Attendee, 'MAGFest schedule, maps, and other FAQs', 'precon_faqs.html',
+               filter=lambda a: True,
+               when=days_before(7, c.EPOCH))
 
 AutomatedEmail(Attendee, 'MAGFest food for guests', 'guest_food_restrictions.txt',
                lambda a: a.badge_type == c.GUEST_BADGE, sender='MAGFest Staff Suite <chefs@magfest.org>')
