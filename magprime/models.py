89ef6cda3795993a14cc0f353e3ba8e1ff83fab6
Victoria Earl <kitsuta@gmail.com>
2016-12-07 10:52:06 -0600

    Calculate group prices based on attendee's prices (#165)
    * Calculate group prices based on attendee's prices
    Fixes https://github.com/magfest/ubersystem/issues/2108, with some caveats.

--- magprime/models.py
+++ magprime/models.py
@@ -2,18 +2,6 @@ from magprime import *
 
 
 @Session.model_mixin
-class Group:
-    @presave_adjustment
-    def bucket_pricing_workaround(self):
-        if not self.is_dealer:
-            if self.auto_recalc:
-                self.cost = self.amount_paid - self.amount_extra
-                self.auto_recalc = False
-            if self.is_new:
-                self.cost = self.cost or self.default_cost
-
-
-@Session.model_mixin
 class Attendee:
     @presave_adjustment
     def invalid_notification(self):
@@ -32,8 +20,10 @@ class Attendee:
 
     @presave_adjustment
     def bucket_pricing_workaround(self):
-        if not self.overridden_price and self.paid in [c.HAS_PAID, c.NOT_PAID]:
-            self.overridden_price = self.badge_cost if self.is_new else self.amount_paid - self.amount_extra
+        if not self.overridden_price:
+            self.overridden_price = self.badge_cost
+        elif self.extra_donation and self.overridden_price == self.default_cost:
+            self.overridden_price -= self.extra_donation
 
     @presave_adjustment
     def child_badge(self):
