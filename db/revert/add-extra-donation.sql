ae849ae532288d6cac4d3ea0e3919aa89899b05d
Rob Ruana <rob@robruana.com>
2017-04-24 12:51:59 -0400

    Cleans up some code, and adds some docs, and adds some initial migrations

--- db/revert/add-extra-donation.sql
+++
@@ -1,8 +0,0 @@
--- Revert magprime:add-extra-donation from pg
-
-BEGIN;
-
-ALTER TABLE attendee
-        DROP COLUMN extra_donation;
-
-COMMIT;
--- db/verify/add-extra-donation.sql
+++
@@ -1,7 +0,0 @@
--- Verify magprime:add-extra-donation on pg
-
-BEGIN;
-
-SELECT column_name FROM information_schema.columns WHERE table_name = 'attendee' AND column_name = 'extra_donation';
-
-ROLLBACK;
