6c2aaaebe61c2b5dc7419a08dfbaebd6bc9acc7d
Victoria Earl <kitsuta@gmail.com>
2017-04-25 21:15:32 -0400

    Move age-related validations that do not use c.CHILD_BADGE
    `c.CHILD_BADGE` is still only used for MAGSuper so we're only moving a couple validations into the general magfest plugin.

--- magprime/model_checks.py
+++ magprime/model_checks.py
@@ -13,20 +13,6 @@ def attendee_badge_under_13(attendee):
         return "You cannot buy an Attendee badge if you will be under 13 at the start of {}".format(c.EVENT_NAME)
 
 
-@prereg_validation.Attendee
-def group_leader_under_13(attendee):
-    if attendee.badge_type == c.PSEUDO_GROUP_BADGE and attendee.age_group_conf['val'] in [c.UNDER_6, c.UNDER_13]:
-        return "Children under 13 cannot be group leaders."
-
-
-@prereg_validation.Attendee
-def total_cost_over_paid(attendee):
-    if attendee.total_cost < attendee.amount_paid:
-        if attendee.orig_value_of('birthdate') < attendee.birthdate and attendee.age_group_conf['val'] in [c.UNDER_6, c.UNDER_13]:
-            return 'The date of birth you entered incurs a discount; please email regsupport@magfest.org to change your badge and receive a refund'
-        return 'You have already paid ${}, you cannot reduce your extras below that.'.format(attendee.amount_paid)
-
-
 @validation.Attendee
 def child_badge_over_18(attendee):
     if attendee.badge_type == c.CHILD_BADGE and attendee.age_group_conf['val'] in [c.UNDER_21, c.OVER_21]:
