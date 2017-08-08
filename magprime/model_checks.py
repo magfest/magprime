ace135128d8e0121c26ee7469ee609ea28d59aa2
kitsuta@gmail.com <kitsuta@gmail.com>
2017-08-08 14:50:43 -0400

    Update validation errors for child badges 1. We no longer call them Child badges 2. The error now tells them what to do to fix the problem

--- magprime/model_checks.py
+++ magprime/model_checks.py
@@ -4,13 +4,13 @@ from magprime import *
 @prereg_validation.Attendee
 def child_badge_over_13(attendee):
     if attendee.is_new and attendee.badge_type == c.CHILD_BADGE and attendee.age_group_conf['val'] not in [c.UNDER_6, c.UNDER_13]:
-        return "You cannot buy a Child badge if you will be 13 or older at the start of {}".format(c.EVENT_NAME)
+        return "If you will be 13 or older at the start of {}, please select an Attendee badge instead of a 12 and Under badge.".format(c.EVENT_NAME)
 
 
 @prereg_validation.Attendee
 def attendee_badge_under_13(attendee):
     if attendee.is_new and attendee.badge_type == c.ATTENDEE_BADGE and attendee.age_group_conf['val'] in [c.UNDER_6, c.UNDER_13]:
-        return "You cannot buy an Attendee badge if you will be under 13 at the start of {}".format(c.EVENT_NAME)
+        return "If you will be 12 or under at the start of {}, please select a 12 and Under badge instead of an Attendee badge.".format(c.EVENT_NAME)
 
 
 @validation.Attendee
