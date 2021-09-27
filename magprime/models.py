33e06f2ca8a5c021bed15d6bf58bfcffd499e761
Victoria Earl <kitsuta@gmail.com>
2021-09-27 19:50:01 -0400

    Don't crash the system if shirt size is out of bounds We've limited our shirt sizes vs prior years and that's causing a 500 error for some staffers. This should hopefully fix that.

--- magprime/models.py
+++ magprime/models.py
@@ -68,7 +68,10 @@ class Attendee:
         if num_staff_shirts_owed > 0:
             staff_shirts = '{} Staff Shirt{}'.format(num_staff_shirts_owed, 's' if num_staff_shirts_owed > 1 else '')
             if self.shirt_size_marked:
-                staff_shirts += ' [{}]'.format(c.SHIRTS[self.shirt])
+                try:
+                    staff_shirts += ' [{}]'.format(c.SHIRTS[self.shirt])
+                except KeyError:
+                    pass
             merch.append(staff_shirts)
 
         if self.staffing:
