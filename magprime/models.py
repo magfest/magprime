de098a5518da8fb63db37dfe17b941d57ca0c543
Victoria Earl <kitsuta@gmail.com>
2021-09-28 13:27:25 -0400

    Use SIZE_UNKNOWN for shirts not in the size list

--- magprime/models.py
+++ magprime/models.py
@@ -71,7 +71,7 @@ class Attendee:
                 try:
                     staff_shirts += ' [{}]'.format(c.SHIRTS[self.shirt])
                 except KeyError:
-                    pass
+                    staff_shirts += ' [{}]'.format(c.SHIRTS[c.SIZE_UNKNOWN])
             merch.append(staff_shirts)
 
         if self.staffing:
