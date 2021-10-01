1bd7724e6d99914d03ca13ef2afd6eb29d4240c6
Victoria Earl <kitsuta@gmail.com>
2021-10-01 00:04:56 -0400

    Revert "Use SIZE_UNKNOWN for shirts not in the size list"
    This causes its own 500 error -- quickly fixing it so people can see their registrations again.

--- magprime/models.py
+++ magprime/models.py
@@ -71,7 +71,7 @@ class Attendee:
                 try:
                     staff_shirts += ' [{}]'.format(c.SHIRTS[self.shirt])
                 except KeyError:
-                    staff_shirts += ' [{}]'.format(c.SHIRTS[c.SIZE_UNKNOWN])
+                    pass
             merch.append(staff_shirts)
 
         if self.staffing:
