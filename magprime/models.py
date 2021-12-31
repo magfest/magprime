5bc6c7302227ac6ca3e3828af2b0c0693200d5ea
Victoria Earl <kitsuta@gmail.com>
2021-12-31 16:31:10 -0500

    Prevent badge donation attendees from checking in Also fixes a JS bug when the shirt size is not displayed on the admin form.

--- magprime/models.py
+++ magprime/models.py
@@ -5,7 +5,7 @@ from uber.config import c
 from uber.decorators import presave_adjustment, render
 from uber.models import Boolean, MagModel, Choice, DefaultColumn as Column, Session
 from uber.tasks.email import send_email
-from uber.utils import add_opt, remove_opt
+from uber.utils import add_opt, check, localized_now, remove_opt
 
 
 @Session.model_mixin
@@ -95,6 +95,38 @@ class Attendee:
             merch.append('Staffer Info Packet')
 
         return merch
+
+    @property
+    def is_not_ready_to_checkin(self):
+        """
+        Returns None if we are ready for checkin, otherwise a short error
+        message why we can't check them in.
+        """
+        
+        if self.badge_status == c.WATCHED_STATUS:
+            if self.banned or not self.regdesk_info:
+                regdesk_info_append = " [{}]".format(self.regdesk_info) if self.regdesk_info else ""
+                return "MUST TALK TO SECURITY before picking up badge{}".format(regdesk_info_append)
+            return self.regdesk_info or "Badge status is {}".format(self.badge_status_label)
+
+        if self.badge_status not in [c.COMPLETED_STATUS, c.NEW_STATUS]:
+            return "Badge status is {}".format(self.badge_status_label)
+        
+        if self.placeholder:
+            return "Placeholder badge"
+
+        if self.is_unassigned:
+            return "Badge not assigned"
+
+        if self.is_presold_oneday:
+            if self.badge_type_label != localized_now().strftime('%A'):
+                return "Wrong day"
+
+        if self.donate_badge_cost:
+            return "Asked badge + merch to be shipped to them"
+
+        message = check(self)
+        return message
 
 
 class SeasonPassTicket(MagModel):
