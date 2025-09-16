6429cda263f1e064211762585088f9b3e5ba5913
Victoria Earl <kitsuta@gmail.com>
2025-09-16 19:27:09 +0200

    Add validation for atrium rooms Prevents people from selecting an atrium room if they don't also select the Gaylord.

--- magprime/validations.py
+++ magprime/validations.py
@@ -2,7 +2,7 @@ from wtforms import validators
 from wtforms.validators import ValidationError, StopValidation
 
 from .config import c
-from uber.validations import TableInfo, BadgeExtras, PanelInfo
+from uber.validations import TableInfo, BadgeExtras, PanelInfo, RoomLottery
 
 
 TableInfo.field_validation.required_fields['prior_name'] = ("Please provide your prior table name.", 'has_prior_name')
@@ -16,3 +16,13 @@ BadgeExtras.field_validation.validations['extra_donation']['minimum'] = validato
 PanelInfo.field_validation.required_fields['broadcast_title'] = ("Please provide a short title for digital displays.",
                                                                  'name', lambda x: len(x) > 40)
 PanelInfo.field_validation.required_fields['broadcast_subtitle'] = "Please provide a one-line summary for digital displays."
+
+
+@RoomLottery.field_validation('room_type_preference')
+def atrium_gaylord_only(form, field):
+    if not field.data or not form.hotel_preference or not form.hotel_preference.data:
+        return
+    
+    if (c.HOTEL_LOTTERY_KING_ATRIUM in field.data or c.HOTEL_LOTTERY_DOUBLE_ATRIUM in field.data
+            ) and c.HOTEL_LOTTERY_GAYLORD not in form.hotel_preference.data:
+        raise ValidationError("Atrium rooms are only available at the Gaylord National Harbor.")
